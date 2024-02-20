af996e0f4aa5e718dd71514e182c5251557022eb
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-20 10:31:29 +0530

    fix(basic-type): starting to learn basic terminology

---
+++ ls01/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = 'ptech12';
+// ÷console.log(username);
+let a = 5;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
--- ls01/src/main.ts
+++ ls01/src/main.ts
@@ -1,4 +1,15 @@
 let username = 'ptech12'
 
-console.log(username);
+// ÷console.log(username);
+let a: number = 5;
+let b: number = 6;
+let c: number = 2;
 
+
+console.log(a / b);
+
+console.log(c * b);
+  
+
+
+ 
