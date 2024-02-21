a7cf499b71ec34f31992c1504bde03c42ecb4700
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-21 09:46:14 +0530

    add basic types in lesson02
    Here I learned how to use basic data types in Typescript

--- ls02/build/js/main.js
+++ ls02/build/js/main.js
@@ -1,1 +1,19 @@
 "use strict";
+let myName; // explict declaration
+let isLoading;
+let album; // any type can be assigned
+let union; // it is union type
+/*
+    Legal operation in JS
+    But TS will not allow
+*/
+myName = 'sing';
+isLoading = true;
+album = true;
+album = 312;
+union = 'Van Helen';
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(5, 6));
+let re = /\w+/g; // RegExp
