ee9917a02c3ba01a3ae182d1d0111df8a3f1c7b2
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-22 22:49:08 +0530

    arrays pending and objects pending

--- ls03/build/js/main.js
+++ ls03/build/js/main.js
@@ -1,1 +1,28 @@
 "use strict";
+// only have string datatype
+let stringArr = ['one', 'two', 'three'];
+// have union datatype => string | number
+let guitars = ['start', 'Les Paul', 5150];
+// multiple union datatype => mixed datatypes
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = '2323';
+stringArr.push('he');
+guitars[0] = 1984;
+guitars.unshift(143);
+// defintly accepts string datatype
+guitars = stringArr;
+// but can't define the other way
+// stringArr = guitar
+let test = [];
+// pre-define the array dataype
+let bands = [];
+// console.log(bands);
+bands.push("Van Helen");
+// console.log(bands);
+// Tuple
+/*
+* Tuple has more strict than normal arrays
+* also define specified length before
+*/
+let myTuple = ['Deva', 43, true];
+let mixed = ['John'];
---
+++ ls03/src/main.ts
@@ -0,0 +1,40 @@
+// only have string datatype
+let stringArr = ['one', 'two', 'three'];
+// have union datatype => string | number
+let guitars = ['start', 'Les Paul', 5150];
+// multiple union datatype => mixed datatypes
+let mixedData =['EVH', 1984, true];
+
+
+stringArr[0] = '2323'
+
+
+stringArr.push('he');
+
+guitars[0] = 1984;
+guitars.unshift(143);
+
+
+// defintly accepts string datatype
+guitars = stringArr
+// but can't define the other way
+// stringArr = guitar
+
+
+let test = []
+// pre-define the array dataype
+let bands: string[] = [];
+
+// console.log(bands);
+bands.push("Van Helen");
+// console.log(bands);
+
+
+// Tuple
+/* 
+* Tuple has more strict than normal arrays
+* also define specified length before
+*/
+let myTuple: [string, number, boolean] = ['Deva', 43, true];
+
+let mixed = ['John']
