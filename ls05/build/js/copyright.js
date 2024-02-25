c13126bd53db34ca583956f4ca6a17ac5c68748d
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-25 20:26:46 +0530

    Chapter 5: assertions completed

---
+++ ls05/build/js/copyright.js
@@ -0,0 +1,20 @@
+"use strict";
+// const year =  document.getElementById("year");
+// const thisYear = new Date().getFullYear();
+// year?.setAttribute("datetime", thisYear);
+// year.textContent = thisYear
+// // 1st variation 
+// const year: HTMLElement | null =  document.getElementById("year");
+// let thisYear: string 
+// thisYear = new Date().getFullYear().toString();
+// // type gaurd
+// if (year) {   
+//     year.setAttribute("datetime", thisYear);
+//     year.textContent = thisYear
+// }
+// 2st variation 
+const year = document.getElementById("year"); // assertions
+const thisYear = new Date().getFullYear().toString();
+// type gaurd 
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
