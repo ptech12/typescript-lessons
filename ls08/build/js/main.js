24a01644d58532f61358e2347087a0b8685ee99b
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-26 23:43:56 +0530

    chapter 8.2: generic with interface and methods

--- ls08/build/js/main.js
+++ ls08/build/js/main.js
@@ -3,15 +3,15 @@
 // work with any type
 const echo = (arg) => arg;
 const isObj = (arg) => {
-    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null);
+    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
 // testing out
 console.log(isObj(true));
-console.log(isObj('John'));
+console.log(isObj("John"));
 console.log(isObj([1, 2, 3])); // Array is object type in JS
-console.log(isObj({ name: 'Hon' }));
+console.log(isObj({ name: "Hon" }));
 console.log(isObj(null));
-console.log('///////');
+console.log("///////");
 // use of generic
 // function need to have some logic
 const isTrue = (arg) => {
@@ -19,8 +19,39 @@ const isTrue = (arg) => {
     if (Array.isArray(arg) && !arg.length) {
         return { arg, is: false };
     }
+    // checking for obj
+    if (isObj(arg)) {
+        return { arg, is: false };
+    }
     return {
         arg,
-        is: !!arg
+        is: !!arg,
+    };
+};
+console.log(isTrue(0));
+console.log(isTrue(true));
+console.log(isTrue(1));
+console.log(isTrue("Dave"));
+console.log(isTrue(""));
+console.log(isTrue(null));
+console.log(isTrue(undefined));
+console.log(isTrue({})); // modified
+console.log(isTrue({ name: "Dave" }));
+console.log(isTrue([])); // modified
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+console.log(isTrue(-0));
+const checkBoolValue = (arg) => {
+    // checking out array
+    if (Array.isArray(arg) && !arg.length) {
+        return { value: arg, is: false };
+    }
+    // checking for obj
+    if (isObj(arg)) {
+        return { value: arg, is: false };
+    }
+    return {
+        value: arg,
+        is: !!arg,
     };
 };
--- ls08/src/main.ts
+++ ls08/src/main.ts
@@ -1,38 +1,79 @@
-
-
 // using generic
 // work with any type
 const echo = <T>(arg: T): T => arg;
 
-
 const isObj = <T>(arg: T): boolean => {
-    return (typeof arg === 'object' && !Array.isArray(arg) && arg !== null)
-}
+  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+};
 
 // testing out
 console.log(isObj(true));
-console.log(isObj('John'));
-console.log(isObj([1,2,3])); // Array is object type in JS
-console.log(isObj({name: 'Hon'}));
+console.log(isObj("John"));
+console.log(isObj([1, 2, 3])); // Array is object type in JS
+console.log(isObj({ name: "Hon" }));
 console.log(isObj(null));
 
-console.log('///////');
+console.log("///////");
 
 // use of generic
 // function need to have some logic
-const isTrue = <T>(arg: T): { arg: T, is: boolean} => {
+const isTrue = <T>(arg: T): { arg: T; is: boolean } => {
+  // checking out array
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: false };
+  }
+  // checking for obj
+  if (isObj(arg)) {
+    return { arg, is: false };
+  }
+  return {
+    arg,
+    is: !!arg,
+  };
+};
+
+console.log(isTrue(0));
+
+console.log(isTrue(true));
+console.log(isTrue(1));
+
+console.log(isTrue("Dave"));
+console.log(isTrue(""));
+
+console.log(isTrue(null));
+
+console.log(isTrue(undefined));
+
+console.log(isTrue({})); // modified
+
+console.log(isTrue({ name: "Dave" }));
+
+console.log(isTrue([])); // modified
+
+console.log(isTrue([1, 2, 3]));
+console.log(isTrue(NaN));
+
+console.log(isTrue(-0));
+
+
+
+ interface BoolCheck<T> {
+    value: T,
+    is: boolean
+ }
+
+
+const checkBoolValue = <T>(arg: T): BoolCheck<T> => {
     // checking out array
-    if(Array.isArray(arg) && !arg.length){
-        return { arg, is: false }
+    if (Array.isArray(arg) && !arg.length) {
+      return { value: arg, is: false };
     }
     // checking for obj
-    if(isObj(arg)) {
-        return { arg, is: false }
+    if (isObj(arg)) {
+      return { value: arg, is: false };
     }
     return {
-        arg,
-        is: !!arg
+      value: arg,
+      is: !!arg,
     }
 }
-
- 
