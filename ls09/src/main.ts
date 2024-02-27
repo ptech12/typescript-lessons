4c953e228a886308db4937edda3934ccb39590f4
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-02-27 22:54:40 +0530

    chapter 9.1: Partial utility type

--- ls09/src/main.ts
+++ ls09/src/main.ts
@@ -1,2 +1,28 @@
-console.log('Lesson 09 start');
-  
+// utility types
+
+// Partial 
+interface Assignment{
+    studentId: string,
+    title: string,
+    grade: number,
+    verified?: boolean
+}
+
+// allow us to update only one part of the Object 
+// with Partial
+const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
+    return { ...assign, ...propsToUpdate}
+}
+
+const assign1: Assignment = {
+    studentId: "vtu12345",
+    title: "Final Project",
+    grade: 0,
+}
+
+console.log(updateAssignment(assign1, { grade: 94 })); 
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 97 })
+
+console.log(assignGraded);
+
+
